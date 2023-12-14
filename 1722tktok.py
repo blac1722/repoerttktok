9c6cc6888e9e29674225e59d998927955cec52b5
blac1722 <153836354+blac1722@users.noreply.github.com>
2023-12-14 09:33:20 -0500

    Add files via upload

---
+++ 1722tktok.py
@@ -0,0 +1,51 @@
+import requests
+from requests.sessions import session
+import json
+import time
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+session = requests.session()
+
+print(Fore.CYAN + """  
+                                                                                                                    
+  ██                                                                        ██  ▀███        ██           ▀███       
+  ██                                        ▄▄▄                             ██    ██        ██             ██       
+██████  ▄▄█▀██ ▄█▀██▄ ▀████████▄█████▄     ▀███ █▄▄▄▄▄▄█▀███▀██▄███▀██▄   ██████  ██  ▄██▀██████  ▄██▀██▄  ██  ▄██▀ 
+  ██   ▄█▀   ███   ██   ██    ██    ██       ██ █     █▀███   ████   ██     ██    ██ ▄█     ██   ██▀   ▀██ ██ ▄█    
+  ██   ██▀▀▀▀▀▀▄█████   ██    ██    ██       ██      █▀     ▄▄██   ▄▄██     ██    ██▄██     ██   ██     ██ ██▄██    
+  ██   ██▄    ▄█   ██   ██    ██    ██       ██     █▀   ▄▄█▀   ▄▄█▀        ██    ██ ▀██▄   ██   ██▄   ▄██ ██ ▀██▄  
+  ▀████ ▀█████▀████▀██▄████  ████  ████▄   ▄████▄  █▀   ███████████████     ▀████████▄ ██▄▄ ▀████ ▀█████▀▄████▄ ██▄▄
+                                                 █▀                                                                 
+                                                █▀                                                                  
+
+ """)
+print("")
+print("")
+print("code by black hat1722")
+print("")
+print("")
+
+x = input('URL tikto kaka lera dabne: ')
+print("")
+print("")
+
+print('Reporting the poor sod.....')
+print('')
+print('')
+
+while True:
+    req = session.post(x)
+    
+    print(req.text)
+    print('reported :D')
+
+    time.sleep(10)
+
+
+input()
+
+
+
